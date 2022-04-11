fdfc99053d17815317217b491c664a79e61de928
suitengumae007 <96781795+suitengumae007@users.noreply.github.com>
2022-04-11 15:37:09 -0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -105,7 +105,7 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 //TODO Add here your network seed nodes
-const std::initializer_list<const char*> SEED_NODES = {
+const std::initializer_list<const char*> SEED_NODES = {192.11.11.11:90210
   //"your_seed_ip1.com:8080",
   //"your_seed_ip2.com:8080",
 };
